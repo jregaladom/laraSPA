a0d2a2826a960c8b92d7df554c8db5157ffc82a4
jose.regalado <jose.regalado@irapuato.gob.mx>
2023-02-09 15:39:46 -0600

    Avance curso

---
+++ resources/js/Pages/Notes/Index.vue
@@ -0,0 +1,58 @@
+
+<template>
+    <AppLayout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Módulo de Notas
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">  
+                <div class="md:grid md:grid-cols-3 md:gap-6">
+                    <div class="md:col-span-1">
+                        <div class="px-4 sm:px0">
+                            <h3 class="text-lg text-gray-900">Listado de notas</h3>
+                            <p class="text-sm text-gray-600">Toma el registro correcto y ejectura cualquier functión (ver, editar o eliminar)</p>
+                        </div>
+                    </div>
+                    <div class="md:col-span-2 mt-5 md:mt-0">
+                        <div class="shadow bg-white md:redounded-md p-4">
+                            <table>
+                                <tr v-for="note in notes" :key="note.id">
+                                    <td class="border px-4 py-2">{{ note.excerpt }}</td>
+                                    <td class="px-4 py-2">
+                                        <Link :href="route('notes.show',note.id)">
+                                            Ver
+                                        </Link>
+                                    </td>
+                                    <td class="px-4 py-2">
+                                        <Link :href="route('notes.edit',note.id)">
+                                            Editar
+                                        </Link>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
+
+<script>
+import AppLayout from '@/Layouts/AppLayout.vue';
+import { Link } from '@inertiajs/vue3'
+
+export default {
+    components: {
+        AppLayout,
+        Link
+    },
+    props:{
+        notes: Array
+    }
+}
+</script>
+
